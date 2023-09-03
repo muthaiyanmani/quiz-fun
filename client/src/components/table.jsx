cfb46743b4d9699eb4abe57e15d3bb8408a0f6bf
hareesh-r <hareeshprogrammer@gmail.com>
2023-09-03 17:59:10 +0530

    fix : change table to dark mode

--- client/src/components/table.jsx
+++ client/src/components/table.jsx
@@ -8,65 +8,75 @@ import { motion, AnimatePresence } from 'framer-motion';
 export default function Table({ header = [], data = [], meta = {}, showLoader = false }) {
   const numberOfRows = 3;
   return (
-    <div className="w-full px-4 mx-auto sm:px-6 lg:px-8">
+    <div className="w-full px-4 mx-auto sm:px-6 lg:px-8 dark:bg-gray-900 dark:text-white">
       {
-        meta?.name && ( <div className="sm:flex sm:items-center">
-        <div className="sm:flex-auto">
-          <h1 className="text-base font-medium leading-6 text-white">{meta.name}</h1>
-          <p className="mt-2 text-sm text-gray-500">
-            {meta.description}
-          </p>
-        </div>
-        <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-          <button
-            type="button"
-            onClick={meta.handleAddAction}
-            className="block px-3 py-2 text-sm font-medium text-center text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            {meta.addButton}
-          </button>
-        </div>
-      </div>)
+        meta?.name && (
+          <div className="sm:flex sm:items-center">
+            <div className="sm:flex-auto">
+              <h1 className="text-base font-medium leading-6">{meta.name}</h1>
+              <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">
+                {meta.description}
+              </p>
+            </div>
+            <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+              <button
+                type="button"
+                onClick={meta.handleAddAction}
+                className="block px-3 py-2 text-sm font-medium text-center bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-indigo-600 dark:bg-indigo-400 dark:hover:bg-indigo-500"
+              >
+                {meta.addButton}
+              </button>
+            </div>
+          </div>
+        )
       }
       <div className="flow-root mt-8">
         <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-300">
-                <thead className="bg-gray-50">
-                <tr>
-                  {header.map((item, index) => (
-                    <th
-                      scope="col"
-                      key={index}
-                      className="px-3 py-3.5 text-left font-medium text-gray-900"
-                    >
-                      {item.label}
-                    </th>
-                  ))}
-                </tr>
+            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg dark:ring-opacity-20">
+              <table className="min-w-full divide-y divide-gray-300 dark:divide-gray-600">
+                <thead className="bg-gray-50 dark:bg-gray-800">
+                  <tr>
+                    {header.map((item, index) => (
+                      <th
+                        scope="col"
+                        key={index}
+                        className="px-3 py-3.5 text-left font-medium text-gray-900 dark:text-white"
+                      >
+                        {item.label}
+                      </th>
+                    ))}
+                  </tr>
                 </thead>
-                <tbody className="bg-white divide-y divide-gray-200 ">
+                <tbody className="bg-white divide-y divide-gray-200 dark:bg-gray-900 dark:divide-gray-600">
+                  {/* Loading Animation Styles */}
                   <AnimatePresence>
-                  {showLoader ?
-                    Array(numberOfRows).fill().map((id, index) => <SkeletonLoader key={index} noOfColoumns={header.length}/>) :
-                    
-                  data.map((item) => (
-                    <motion.tr
-                      key={item.id}
-                      layout
-                      initial={{ opacity: 0 }}
-                      animate={{ opacity: 1 }}
-                      exit={{ opacity: 0 }}
-                    >
-                      {header.map((headerItem, index) => (
-                        <td key={index} className="px-3 py-4 text-sm text-gray-500 md:text-md whitespace-nowrap">
-                          {headerItem.key === 'link' ?  <Link to={item[headerItem.key]}><ArrowTopRightOnSquareIcon className="inline w-4 h-4 font-bold"/> </Link> : item[headerItem.key]  }
+                  {showLoader ? (
+                    Array(numberOfRows).fill().map((id, index) => (
+                      <tr key={index}>
+                        <td className="px-3 py-4 animate-pulse" colSpan={header.length}>
+                          <SkeletonLoader key={index} noOfColoumns={header.length}/>
                         </td>
-                      ))}
-                    </motion.tr>
-                  ))
-                }
+                      </tr>
+                    ))
+                  ) : (
+                    /* Data Rows */
+                    data.map((item) => (
+                      <motion.tr
+                        key={item.id}
+                        layout
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                      >
+                        {header.map((headerItem, index) => (
+                          <td key={index} className="px-3 py-4 text-sm text-gray-400 md:text-md whitespace-nowrap">
+                            {headerItem.key === 'link' ? <Link to={item[headerItem.key]}><ArrowTopRightOnSquareIcon className="inline w-4 h-4 font-bold" /> </Link> : item[headerItem.key]}
+                          </td>
+                        ))}
+                      </motion.tr>
+                    ))
+                  )}
                   </AnimatePresence>
                 </tbody>
               </table>
@@ -75,11 +85,18 @@ export default function Table({ header = [], data = [], meta = {}, showLoader = 
         </div>
       </div>
     </div>
+
+
   );
 }
 
-const SkeletonLoader = ({noOfColoumns=1}) => (
-    <tr>
-      { Array(noOfColoumns).fill().map( (id, index) => (<td key={index} className="px-3 py-4 text-sm text-gray-500 whitespace-nowrap skeleton none"></td>))}  
-    </tr>
-)
+const SkeletonLoader = ({ noOfColumns = 1 }) => (
+  <tr>
+    {Array(noOfColumns).fill().map((id, index) => (
+      <td
+        key={index}
+        className="px-3 py-4 text-sm text-gray-500 md:text-md whitespace-nowrap skeleton none"
+      ></td>
+    ))}
+  </tr>
+);
